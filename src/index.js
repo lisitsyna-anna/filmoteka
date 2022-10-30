e8fd3db44973606c0ce5c25770e9edb752815e79
lia2307 <juliatymchuk23@gmail.com>
2022-10-30 20:48:24 +0200

    add js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { renderModalMovie } from './js/API/get-movie-info';
 
 import { onFormSubmit } from './js/gallery';
 
+import './js/switch-bg-theme';
+
 ///Запуск спінера при завантаженні
 spinnerPlay();
 window.addEventListener('load', spinnerStop);
