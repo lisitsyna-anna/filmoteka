36fb9e4565735734963b95f0b05af2c4df89f771
olhazamlynska <olya.sonechko@gmail.com>
2022-10-30 16:21:03 +0200

    Add spinner

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+import { spinnerPlay, spinnerStop } from './js/loader';
 import { renderTrendingMovies } from './js/API/get-trending';
 import { onFormSubmit } from './js/gallery';
+
+///Запуск спінера при завантаженні
+spinnerPlay();
+window.addEventListener('load', spinnerStop);
+///Закінчення спінера при завантаженні
