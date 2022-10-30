8beeceaf2a8ee1faa6087eed5f77b1534d4fd102
Anna Lisitsyna <anna.lisitsyna93@gmail.com>
2022-10-30 17:39:03 +0100

    work with local storage

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,6 +2,7 @@ export const refs = {
   galleryMovies: document.querySelector('.gallery__list'),
   formSearch: document.querySelector('.header__form'),
   modal: document.querySelector('.modal__content'),
+  modalContainer: document.querySelector('.modal'),
   modalBackdrop: document.querySelector('.backdrop'),
   closeModalBtn: document.querySelector('.modal-close'),
   notification: document.querySelector('.header__notification'),
