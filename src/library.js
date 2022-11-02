bb21a038c07131553b5ccac3a6926f0377423ea3
IreneCreadora <nargiza.dancer@gmail.com>
2022-11-02 17:02:00 +0200

    feature library.js

---
+++ src/library.js
@@ -0,0 +1,34 @@
+import { spinnerPlay, spinnerStop } from './js/spinner';
+import { renderTrendingMovies } from './js/API/get-trending';
+
+import { renderModalMovie } from './js/API/get-movie-info';
+
+import { onFormSubmit } from './js/gallery';
+import { openModalTeam } from './js/open-modal-team';
+
+import './js/switch-bg-theme';
+
+import { scrollFunction } from './js/button-up';
+
+import { onCliсkBtnWatch } from './js/API/get-movie-trailer';
+import { onCliсkBtnWatchGallery } from './js/API/get-movie-trailer';
+
+import { onOpenQueueLibrary } from './js/queue-library';
+
+import { onOpenWatchedLibrary } from './js/watched-library';
+
+import { loadWatchedMoviesFromLocalStorage } from './js/watched-library';
+import { onLibraryGallery } from './js/open-modal-library';
+
+///Запуск спінера при завантаженні
+spinnerPlay();
+window.addEventListener('load', function (e) {
+  spinnerStop();
+});
+///Закінчення спінера при завантаженні
+
+///Запуск кнопки вверх при скролі
+window.addEventListener('scroll', scrollFunction);
+
+// При завантаженні сторінки My library рендериться картки з локал сторадж watchedMovies
+window.addEventListener('load', loadWatchedMoviesFromLocalStorage);
